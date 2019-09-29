f65efea50f0b722710fab4378311da4b6be2b0a4
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2019-09-29 22:33:06 +0800

    add animation

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 import jump from './node_modules/jump.js/dist/jump.module.js';
+// import sal from './node_modules/sal.js/dist/sal.js';
+// const sal = require('./node_modules/sal.js');
 
 const project = document.querySelector('.project');
+const landing = document.querySelector('.landing');
 const btnScrollDown = document.querySelector('.scroll-down');
+const btnUpArrow = document.querySelector('.up-arrow');
+
+sal({
+  once: false,
+});
 
 btnScrollDown.addEventListener('click', () => {
   jump(project, {
+    duration: 1800,
+    offset: 0,
+    callback: undefined,
+    a11y: false 
+  });
+});
+
+btnUpArrow.addEventListener('click', () => {
+  jump(landing, {
     duration: 1800,
     offset: 0,
     callback: undefined,

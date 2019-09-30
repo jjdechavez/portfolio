196099232b26998a46eebc600f87dcc5fa7c2058
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2019-09-30 11:37:58 +0800

    change src

---
+++ dist/app.js
@@ -0,0 +1,28 @@
+import jump from 'https://jjdechavez.netlify.com/node_modules/jump.js/dist/jump';
+
+const project = document.querySelector('.project');
+const landing = document.querySelector('.landing');
+const btnScrollDown = document.querySelector('.scroll-down');
+const btnUpArrow = document.querySelector('.up-arrow');
+
+sal({
+  once: false,
+});
+
+btnScrollDown.addEventListener('click', () => {
+  jump(project, {
+    duration: 1800,
+    offset: 0,
+    callback: undefined,
+    a11y: false 
+  });
+});
+
+btnUpArrow.addEventListener('click', () => {
+  jump(landing, {
+    duration: 1800,
+    offset: 0,
+    callback: undefined,
+    a11y: false 
+  });
+});

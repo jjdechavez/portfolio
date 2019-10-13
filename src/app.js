4d62d844ba43541e6d78a66b3de139de5c42936d
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2019-10-13 22:49:39 +0800

    added scroll

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import jump from 'jump.js';
+import sal from 'sal.js';
+// import './node_modules/sal.js/dist/sal.css';
+import './styles/style.scss';
+
+const project = document.querySelector('.project');
+const landing = document.querySelector('.landing');
+const btnScrollDown = document.querySelector('.scroll-down');
+const btnUpArrow = document.querySelector('.up-arrow');
+
+sal({
+  once: false,
+});
+
+btnScrollDown.addEventListener('click', () => {
+  jump(project, {
+    duration: 1800,
+    offset: 0,
+    callback: undefined,
+    a11y: false 
+  });
+});
+
+btnUpArrow.addEventListener('click', () => {
+  jump(landing, {
+    duration: 1800,
+    offset: 0,
+    callback: undefined,
+    a11y: false 
+  });
+});

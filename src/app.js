cb42f152a62ddf1ca692eaf5cc0472234cb246cb
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2019-10-25 00:32:57 +0800

    final update

--- src/app.js
+++ src/app.js
@@ -1,44 +1,20 @@
-// import jump from './script/jump';
-// import sal from 'sal.js';
-// import './styles/style.scss';
+import jump from './script/jump';
+import sal from 'sal.js';
+import './styles/style.scss';
 
 const project = document.querySelector('.project');
 const btnScrollDown = document.querySelector('.scroll-down');
 const element = (element) => document.querySelector(element);
 
-// sal({
-//   once: true,
-// });
+sal({
+  once: true,
+});
 
-// btnScrollDown.addEventListener('click', () => {
-//   jump(project, {
-//     duration: 1800,
-//     offset: 0,
-//     callback: undefined,
-//     a11y: false
-//   });
-// });
-
-
-// function viewportChange(x) {
-//   if (x.matches) { 
-//     let click = false;
-
-//     element('.open').addEventListener('click', () => {
-//       click = !click;
-//       if (click === false) {
-//         element('.detail-img').style.display = 'none';
-//         element('.more-details').style.display = 'inherit';
-//       } else {
-//         element('.detail-img').style.display = 'inherit';
-//         element('.more-details').style.display = 'none';
-//       }
-//     });
-//   } else {
-//     element('.open').removeEventListner('click');
-//   }
-// }
-
-// var x = window.matchMedia("(min-width: 587px)")
-// viewportChange(x) // Call listener function at run time
-// x.addListener(viewportChange) // Attach listener function on state changes
+btnScrollDown.addEventListener('click', () => {
+  jump(project, {
+    duration: 1800,
+    offset: 0,
+    callback: undefined,
+    a11y: false
+  });
+});

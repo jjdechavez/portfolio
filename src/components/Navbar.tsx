c7d5627ff7fa482d4aa392339f8458474934078c
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2021-05-02 16:12:51 +0800

    Added SideBar and update Dashboard with cards

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,143 +1,41 @@
 import React from 'react';
-import Link from 'next/link';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faBars,
-  faTv,
-  faNewspaper,
-  faTimes,
-  faClipboardList,
-  faTools,
-} from '@fortawesome/free-solid-svg-icons';
 
 import AccountDropdown from './AccountDropdown';
 
 export default function Navbar() {
-  const [collapseShow, setCollapseShow] = React.useState('hidden');
   return (
     <>
-      <nav
-        className="
-          md:left-0 md:block md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden md:w-64
-          shadow-xl bg-white flex flex-wrap items-center justify-between relative z-10 py-4 px-6
-        "
-      >
-        <div className="md:flex-col md:items-stretch md:min-h-full md:flex-nowrap flex flex-wrap items-center justify-between w-full mx-auto">
-          {/* Toggler */}
-          <button
-            className="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
-            type="button"
-            onClick={() => setCollapseShow('bg-white m-2 py-3 px-6')}
-          >
-            <FontAwesomeIcon icon={faBars} />
-          </button>
+      {/* Navbar */}
+      <nav className="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start flex items-center p-4">
+        <div className="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
           {/* Brand */}
-          <div className="md:block text-left md:pb-2 text-gray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0">
-            <Link href="/dashboard">
-              <a>Dashboard</a>
-            </Link>
-          </div>
-          {/* User */}
-          <ul className="md:hidden items-center flex flex-wrap list-none">
-            <li className="inline-block relative">
-              <AccountDropdown />
-            </li>
-          </ul>
-        </div>
-        {/* Collapse */}
-        <div
-          className={
-            'md:flex md:flex-col md:items-stretch md:opacity-100 md:relative md:mt-4 md:shadow-none shadow absolute top-0 left-0 right-0 z-40 overflow-y-auto overflow-x-hidden h-auto items-center flex-1 rounded ' +
-            collapseShow
-          }
-        >
-          <div className="md:min-w-full md:hidden block pb-4 mb-4 border-b border-solid border-blueGray-200">
-            <div className="flex flex-wrap">
-              <div className="w-6/12">
-                <div className="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0">
-                  <Link href="/dashboard">Dashboard</Link>
-                </div>
-              </div>
-              <div className="w-6/12 flex justify-end">
-                <button
-                  type="button"
-                  className="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
-                  onClick={() => setCollapseShow('hidden')}
-                >
-                  <FontAwesomeIcon icon={faTimes} />
-                </button>
-              </div>
-            </div>
-          </div>
-          {/*Form*/}
-          <form className="mt-6 mb-4 md:hidden">
-            <div className="mb-3 pt-0">
+          <a
+            className="text-white text-sm uppercase hidden lg:inline-block font-semibold"
+            href="#pablo"
+            onClick={(e) => e.preventDefault()}
+          >
+            Dashboard
+          </a>
+          {/* Form */}
+          <form className="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3">
+            <div className="relative flex w-full flex-wrap items-stretch">
+              <span className="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
+                <i className="fas fa-search"></i>
+              </span>
               <input
                 type="text"
-                placeholder="Search"
-                className="border-0 px-3 py-2 h-12 border border-solid  border-blueGray-500 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-base leading-snug shadow-none outline-none focus:outline-none w-full font-normal"
+                placeholder="Search here..."
+                className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring w-full pl-10"
               />
             </div>
           </form>
-          {/* Navigation */}
-          <ul className="md:flex-col md:min-w-full flex flex-col list-none">
-            <li className="items-center">
-              <div className="text-pink-500 hover:text-pink-600 text-xs uppercase py-3 font-bold block">
-                <Link href="/dashboard">
-                  <a>
-                    <FontAwesomeIcon icon={faTv} /> Dashboard
-                  </a>
-                </Link>
-              </div>
-            </li>
-            <li className="items-center">
-              <div className="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block">
-                <Link href="/">
-                  <a>
-                    <FontAwesomeIcon icon={faNewspaper} /> Projects
-                  </a>
-                </Link>
-              </div>
-            </li>
-
-            <li className="items-center">
-              <a
-                className="text-blueGray-300 text-xs uppercase py-3 font-bold block"
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                <FontAwesomeIcon icon={faClipboardList} /> Register (soon)
-              </a>
-            </li>
-
-            <li className="items-center">
-              <a
-                className="text-blueGray-300 text-xs uppercase py-3 font-bold block"
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                <FontAwesomeIcon icon={faTools} /> Settings (soon)
-              </a>
-            </li>
-          </ul>
-          {/* Divider */}
-          <hr className="my-4 md:min-w-full" />
-          {/* Heading */}
-          <h6 className="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline">
-            Documentation
-          </h6>
-          {/* Navigation */}
-          <ul className="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
-            <li className="inline-flex">
-              <div className="text-blueGray-700 hover:text-blueGray-500  text-sm block mb-4 no-underline font-semibold">
-                <Link href="/dashboard">
-                  <a>Logout</a>
-                </Link>
-              </div>
-            </li>
+          {/* User */}
+          <ul className="flex-col md:flex-row list-none items-center hidden md:flex">
+            <AccountDropdown />
           </ul>
         </div>
       </nav>
+      {/* End Navbar */}
     </>
   );
 }
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,9 +3,47 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import useUser from '@/hooks/useUser';
 import fetchJson from '@/presentation/helpers/fetchJson';
+import Sidebar from '@/components/Sidebar';
 import Navbar from '@/components/Navbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp, faChartBar } from '@fortawesome/free-solid-svg-icons';
 
 export type HTMLButtonMouseEvent = MouseEvent<HTMLButtonElement>;
+
+function Cards() {
+  return (
+    <div className="flex flex-wrap">
+      <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
+        <div className="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+          <div className="flex-auto p-4">
+            <div className="flex flex-wrap">
+              <div className="relative w-full pr-4 max-w-full flex-grow flex-1">
+                <h5 className="text-gray-400 uppercase font-bold text-xs">
+                  Traffic
+                </h5>
+                <span className="font-semibold text-xl text-blueGray-700">
+                  350,897
+                </span>
+              </div>
+              <div className="relative w-auto pl-4 flex-initial">
+                <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full bg-red-500">
+                  <i className="far fa-chart-bar"></i>
+                  <FontAwesomeIcon icon={faChartBar} />
+                </div>
+              </div>
+            </div>
+            <p className="text-sm text-gray-400 mt-4">
+              <span className="text-green-500 mr-2">
+                <FontAwesomeIcon icon={faArrowUp} /> 3.48%
+              </span>
+              <span className="whitespace-nowrap">Since last month</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default function Dashboard() {
   const { user, mutateUser } = useUser({ redirectTo: '/login' });
@@ -21,8 +59,17 @@ export default function Dashboard() {
 
   return (
     <div>
-      <Navbar />
-      <p>LoggedIn as {user.username}</p>
+      <Sidebar />
+      <div className="relative md:ml-64 bg-gray-100">
+        <Navbar />
+        <div className="relative bg-gray-600 md:pt-32 pb-32 pt-12">
+          <div className="px-4 md:px-10 mx-auto w-full">
+            <div>
+              <Cards />
+            </div>
+          </div>
+        </div>
+      </div>
       <Link href={`/dashboard/posts`}>
         <a>Posts</a>
       </Link>

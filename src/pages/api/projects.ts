da5ff544605bdb43ee8130eaf5a923ad10f4c6f2
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2021-05-15 18:24:47 +0800

    convert api/projects with next-connect

--- src/pages/api/projects.ts
+++ src/pages/api/projects.ts
@@ -1,3 +1,5 @@
+import { NextApiResponse } from 'next';
+import nc from 'next-connect';
 import {
   makeCreateProjectFactory,
   makeGetProjectsFactory,
@@ -6,41 +8,38 @@ import withSession, {
   NextApiRequestWithSession,
 } from '@/infra/session/iron-session';
 import { projectDocumentParse } from '@/presentation/helpers';
-import { ObjectID } from 'bson';
-import { NextApiResponse } from 'next';
 
-export default withSession(
-  async (req: NextApiRequestWithSession, res: NextApiResponse) => {
-    const { method, session, body } = req;
+const handler = nc();
 
-    switch (method) {
-      case 'GET':
-        const getProductsUseCase = await makeGetProjectsFactory();
-        const projects = await getProductsUseCase.projects();
+handler.get(async (req: NextApiRequestWithSession, res: NextApiResponse) => {
+  const getProductsUseCase = await makeGetProjectsFactory();
+  const projects = await getProductsUseCase.projects();
 
-        res.json({ projects });
-        break;
-      case 'POST':
-        const user = session.get('user');
+  res.json({ projects });
+});
 
-        if (!user) {
-          res.status(403).send({ message: 'Invalid to access this method' });
-          return;
-        }
+handler.post(
+  withSession(async (req: NextApiRequestWithSession, res: NextApiResponse) => {
+    const { session, body } = req;
 
-        const projectDocumentParsed = projectDocumentParse(body);
+    const user = session.get('user');
 
-        const createProjectUseCase = await makeCreateProjectFactory();
-        const hasCreatedProject = await createProjectUseCase.createProject(
-          projectDocumentParsed
-        );
-
-        res
-          .status(hasCreatedProject ? 201 : 400)
-          .json({ created: hasCreatedProject });
-      default:
-        res.setHeader('Allow', ['GET', 'POST']);
-        res.status(405).end(`Method ${method} Not Allowed`);
+    if (!user) {
+      res.status(403).send({ message: 'Invalid to access this method' });
+      return;
     }
-  }
+
+    const projectDocumentParsed = projectDocumentParse(body);
+
+    const createProjectUseCase = await makeCreateProjectFactory();
+    const hasCreatedProject = await createProjectUseCase.createProject(
+      projectDocumentParsed
+    );
+
+    res
+      .status(hasCreatedProject ? 201 : 400)
+      .json({ created: hasCreatedProject });
+  })
 );
+
+export default handler;

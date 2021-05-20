bd2f70e42424d616e42821c4680ff8d095ac7968
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2021-05-20 15:11:51 +0800

    Add project form

--- src/pages/dashboard/projects/index.tsx
+++ src/pages/dashboard/projects/index.tsx
@@ -1,19 +1,7 @@
 import Link from 'next/link';
 import { fetchServer } from '@/presentation/helpers/fetchServer';
-
-export default function Projects({ projects }) {
-  // const data = useProjectIds();
-
-  console.log('projects:', projects);
-  return (
-    <div>
-      <h1>My Projects</h1>
-      <Link href={`/dashboard`}>
-        <a>Dashboard</a>
-      </Link>
-    </div>
-  );
-}
+import Admin from '@/layouts/Admin';
+import ProjectForm from '@/components/dashboard/projects/Form';
 
 export async function getStaticProps() {
   const res = await fetchServer('projects');
@@ -25,3 +13,25 @@ export async function getStaticProps() {
     },
   };
 }
+
+export default function Projects({ projects }) {
+  // const data = useProjectIds();
+
+  console.log('projects:', projects);
+  return (
+    <div>
+      <h1>My Projects</h1>
+      <Link href={`/dashboard`}>
+        <a>Dashboard</a>
+      </Link>
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-8/12 px-4">
+          <ProjectForm />
+        </div>
+        <div className="w-full lg:w-4/12 px-4"></div>
+      </div>
+    </div>
+  );
+}
+
+Projects.layout = Admin;

9b0409ed8396d775b612641e4e5856af7a8662a2
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2022-01-15 13:17:06 +0800

    Added santi project

--- src/app.js
+++ src/app.js
@@ -1,20 +1,18 @@
-import jump from './script/jump';
-import sal from 'sal.js';
-import './styles/style.scss';
+import sal from "sal.js";
+import jump from "jump.js";
 
-const project = document.querySelector('.project');
-const btnScrollDown = document.querySelector('.scroll-down');
-const element = (element) => document.querySelector(element);
+const project = document.querySelector(".project");
+const btnScrollDown = document.querySelector(".scroll-down");
 
 sal({
   once: true,
 });
 
-btnScrollDown.addEventListener('click', () => {
+btnScrollDown.addEventListener("click", () => {
   jump(project, {
     duration: 1800,
     offset: 0,
     callback: undefined,
-    a11y: false
+    a11y: false,
   });
 });

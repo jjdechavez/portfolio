4a8183c69ef80b5fcae71ee883923a5a2260f841
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2023-04-08 18:26:27 +0800

    Upgrade packages - update displaying projects

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
-import sal from "sal.js";
-import jump from "jump.js";
+import sal from 'sal.js';
+import jump from 'jump.js';
+import projects from './static/projects.json';
 
-const project = document.querySelector(".project");
-const btnScrollDown = document.querySelector(".scroll-down");
+const jumpContainer = document.querySelector('.project');
+const btnScrollDown = document.querySelector('.scroll-down');
 
-sal({
+const Sal = sal({
   once: true,
 });
 
 if (btnScrollDown) {
-  btnScrollDown.addEventListener("click", () => {
-    jump(project, {
+  btnScrollDown.addEventListener('click', () => {
+    jump(jumpContainer, {
       duration: 1800,
       offset: 0,
       callback: undefined,
@@ -18,3 +19,40 @@ if (btnScrollDown) {
     });
   });
 }
+
+console.log(import.meta.url)
+console.log(new URL('./static/Arsenal.webp', import.meta.url))
+
+const projectList = document.querySelector('#projects');
+
+projects
+  .filter((project) => project.type === 'EXPERCIENCE')
+  .map((project) => {
+    const tile = document.createElement('div');
+    tile.setAttribute('data-sal', 'slide-up');
+    tile.setAttribute('data-sal-duration', '1200');
+    tile.setAttribute('data-sal-delay', '300');
+    tile.setAttribute('data-sal-easing', 'ease-out-bounce');
+    tile.setAttribute('class', 'project-grid-item project-flex-item');
+
+    tile.innerHTML = `
+      <a href="${project.link}" target="_blank" rel="noopener">
+        <img class="detail-img" src="${new URL(project.coverImage, import.meta.url).href}" alt="${
+      project.name
+    }">
+      </a>
+      <div class="project-detail project-flex-about">
+        <p class="project-title">
+          ${project.name}
+          <span class="project-background">
+            ${new Date(project.endedAt).getFullYear().toString()}
+          </span>
+        </p>
+        <p class="more-details">${project.description}</p>
+        <p class="project-background">${project.technologies.join(', ')}</p>
+      </div>
+    `;
+    projectList.appendChild(tile);
+  });
+
+Sal.update();

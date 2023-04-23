34cf039643da21799ddbf3ab15ef3ac5a5bc79b3
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2023-04-23 13:55:53 +0800

    Add ProjectList api and implemented on Home page

---
+++ src/Api/ProjectList.elm
@@ -0,0 +1,86 @@
+module Api.ProjectList exposing (Project, ProjectType(..), getProjectsByType)
+
+import Effect exposing (Effect)
+import Http
+import Json.Decode
+
+
+type ProjectType
+    = Expercience
+    | Personal
+    | All
+
+
+type alias Project =
+    { slug : String
+    , name : String
+    , description : String
+    , technologies : List String
+    , link : String
+    , coverImage : String
+    , endedAt : String
+    , projectType : ProjectType
+    }
+
+
+getProjectsByType :
+    -- ProjectType
+    -- ->
+    { onResponse : Result Http.Error (List Project) -> msg
+    }
+    -> Effect msg
+getProjectsByType options =
+    let
+        -- currentProjectType : String
+        -- currentProjectType =
+        --     case projectType of
+        --         Expercience ->
+        --             "EXPERCIENCE"
+        --
+        --         Personal ->
+        --             "PERSONAL"
+        cmd : Cmd msg
+        cmd =
+            Http.get
+                { url = "/data/projects.json"
+                , expect = Http.expectJson options.onResponse decoder
+                }
+    in
+    Effect.sendCmd cmd
+
+
+decoder : Json.Decode.Decoder (List Project)
+decoder =
+    Json.Decode.list projectDecoder
+
+
+initProjectType : String -> ProjectType
+initProjectType projectType =
+    case projectType of
+        "EXPERCIENCE" ->
+            Expercience
+
+        "PERSONAL" ->
+            Personal
+
+        _ ->
+            All
+
+
+projectTypeDecoder : Json.Decode.Decoder ProjectType
+projectTypeDecoder =
+    Json.Decode.map initProjectType
+        (Json.Decode.field "type" Json.Decode.string)
+
+
+projectDecoder : Json.Decode.Decoder Project
+projectDecoder =
+    Json.Decode.map8 Project
+        (Json.Decode.field "slug" Json.Decode.string)
+        (Json.Decode.field "name" Json.Decode.string)
+        (Json.Decode.field "description" Json.Decode.string)
+        (Json.Decode.field "technologies" (Json.Decode.list Json.Decode.string))
+        (Json.Decode.field "link" Json.Decode.string)
+        (Json.Decode.field "coverImage" Json.Decode.string)
+        (Json.Decode.field "endedAt" Json.Decode.string)
+        projectTypeDecoder
--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,16 +1,98 @@
-module Pages.Home_ exposing (page)
+module Pages.Home_ exposing (Model, Msg, page)
 
+import Api
+import Api.ProjectList exposing (Project, ProjectType)
+import Effect exposing (Effect)
 import Html exposing (Html)
 import Html.Attributes as Attr
+import Http
+import List
+import Page exposing (Page)
+import Route exposing (Route)
+import Shared
 import View exposing (View)
 
 
-page : View msg
-page =
+page : Shared.Model -> Route () -> Page Model Msg
+page _ _ =
+    Page.new
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- INIT
+
+
+type alias Model =
+    { projects : Api.Data (List Project)
+    , showcase : ProjectType
+    }
+
+
+init : () -> ( Model, Effect Msg )
+init () =
+    ( { projects = Api.Loading
+      , showcase = Api.ProjectList.Expercience
+      }
+    , Api.ProjectList.getProjectsByType
+        { onResponse = ProjectApiResponded }
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ProjectApiResponded (Result Http.Error (List Project))
+
+
+update : Msg -> Model -> ( Model, Effect Msg )
+update msg model =
+    case msg of
+        ProjectApiResponded (Ok listOfProjects) ->
+            let
+                expercienceProject : Project -> Bool
+                expercienceProject project =
+                    project.projectType == model.showcase
+
+                projects : List Project
+                projects =
+                    List.filter expercienceProject listOfProjects
+            in
+            ( { model | projects = Api.Success projects }
+            , Effect.none
+            )
+
+        ProjectApiResponded (Err httpError) ->
+            ( model
+            , Effect.none
+            )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> View Msg
+view model =
     { title = "John Jerald De Chavez"
     , body =
         [ viewHeader
-        , viewBody
+        , viewBody model
         , viewFooter
         ]
     }
@@ -62,14 +144,63 @@ viewCTA =
         ]
 
 
-viewBody : Html msg
-viewBody =
-    Html.main_ [ Attr.class "container" ]
-        [ Html.div
-            [ Attr.id "projects"
-            , Attr.class "grid"
+viewProject : Project -> Html msg
+viewProject project =
+    Html.article
+        [ Attr.class "project-grid-item"
+        , Attr.attribute "data-sal" "slide-up"
+        , Attr.attribute "data-sal-duration" "1200"
+        , Attr.attribute "data-sal-delay" "300"
+        , Attr.attribute "data-sal-easing" "ease-out-bounce"
+        ]
+        [ Html.header []
+            [ Html.a
+                [ Attr.href project.link
+                , Attr.target "_blank"
+                , Attr.rel "noopener"
+                ]
+                [ Html.img
+                    [ Attr.class "detail-img"
+                    , Attr.src project.coverImage
+                    , Attr.alt project.name
+                    ]
+                    []
+                ]
             ]
-            []
+        , Html.div [ Attr.class "content" ]
+            [ Html.h3 []
+                [ Html.text project.name
+                , Html.small [] [ Html.text project.endedAt ]
+                ]
+            , Html.p [ Attr.class "description" ]
+                [ Html.text project.description ]
+            , Html.p [ Attr.class "technology" ]
+                [ Html.text <| String.join ", " project.technologies ]
+            ]
+        ]
+
+
+viewProjects : List Project -> List (Html msg)
+viewProjects listOfProjects =
+    List.map viewProject listOfProjects
+
+
+viewBody : Model -> Html msg
+viewBody model =
+    Html.main_ [ Attr.class "container" ]
+        [ case model.projects of
+            Api.Loading ->
+                Html.div [] [ Html.text "Loading..." ]
+
+            Api.Success listOfProjects ->
+                Html.div
+                    [ Attr.id "projects"
+                    , Attr.class "grid"
+                    ]
+                    (viewProjects listOfProjects)
+
+            Api.Failure httpError ->
+                Html.div [] [ Html.text (Api.toUserFriendlyMessage httpError) ]
         ]
 
 

17313d02e9256df89bcba14717fa1473bd8a77c0
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2023-09-09 15:59:37 +0800

    Updated reference

--- src/main.c
+++ src/main.c
@@ -7,14 +7,7 @@
 #include <time.h>
 
 /*
-Copyright (c) 2022-2023 Devine Lu Linvega
-
-Permission to use, copy, modify, and distribute this software for any
-purpose with or without fee is hereby granted, provided that the above
-copyright notice and this permission notice appear in all copies.
-
-THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-WITH REGARD TO THIS SOFTWARE.
+   Inspired and reference to HundredRabbits: https://100r.co/
 */
 
 #define NAME "John Jerald De Chavez"
